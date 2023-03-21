f1e8c7409b48bfadfde72aa95dac28d70879e614
Vaishakh Anil <vaishakh17@gmail.com>
2023-03-22 02:10:40 +0530

    add hidden class to display nothing

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const score01El = document.getElementById('score--0');
 const score02el = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
 
 score01El.textContent = 0;
 score02el.textContent = 0;
+diceEl.classList.add('hidden');
