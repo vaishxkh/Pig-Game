6bad0497906f9b666877dba5a24dcf282b30ac89
Vaishakh Anil <vaishakh17@gmail.com>
2023-03-22 01:55:19 +0530

    inital commit to start out

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const score01El = document.getElementById('score--0');
+const score02el = document.querySelector('#score--1');
+
+score01El.textContent = 0;
+score02el.textContent = 0;
